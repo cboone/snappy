2fba97df0d57774fdb3b99b65a8c2aef37dd5df2
Christopher Boone <cboone@users.noreply.github.com>
2026-03-02 17:38:05 -0500

    fix: print version subcommand output to stdout

--- cmd/version.go
+++ cmd/version.go
@@ -1,13 +1,17 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of snappy",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, _ []string) {
-		cmd.Printf("snappy version %s\n", rootCmd.Version)
+		fmt.Fprintf(cmd.OutOrStdout(), "snappy version %s\n", rootCmd.Version)
 	},
 }
 
