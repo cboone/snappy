591c1efa2f45f177cdad45aa16e9a6330bb1ae6c
Christopher Boone <cboone@users.noreply.github.com>
2026-03-24 09:58:17 -0400

    feat: add column table builder for aligned CLI output (#96)

---
+++ cmd/table.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type columnAlign int
+
+const (
+	alignLeft columnAlign = iota
+	alignRight
+)
+
+type columnDef struct {
+	title string
+	align columnAlign
+}
+
+type columnTable struct {
+	cols []columnDef
+	rows [][]string
+}
+
+func (ct *columnTable) addRow(cells ...string) {
+	if len(cells) != len(ct.cols) {
+		panic(fmt.Sprintf("columnTable: got %d cells, want %d", len(cells), len(ct.cols)))
+	}
+	ct.rows = append(ct.rows, cells)
+}
+
+func (ct *columnTable) render(w io.Writer, showHeader bool) error {
+	widths := make([]int, len(ct.cols))
+	for i, col := range ct.cols {
+		widths[i] = len(col.title)
+	}
+	for _, row := range ct.rows {
+		for i, cell := range row {
+			if len(cell) > widths[i] {
+				widths[i] = len(cell)
+			}
+		}
+	}
+
+	if showHeader {
+		if err := ct.writeLine(w, ct.headerCells(), widths); err != nil {
+			return err
+		}
+	}
+	for _, row := range ct.rows {
+		if err := ct.writeLine(w, row, widths); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ct *columnTable) headerCells() []string {
+	cells := make([]string, len(ct.cols))
+	for i, col := range ct.cols {
+		cells[i] = col.title
+	}
+	return cells
+}
+
+func (ct *columnTable) writeLine(w io.Writer, cells []string, widths []int) error {
+	parts := make([]string, len(cells))
+	for i, cell := range cells {
+		if ct.cols[i].align == alignRight {
+			parts[i] = fmt.Sprintf("%*s", widths[i], cell)
+		} else {
+			parts[i] = fmt.Sprintf("%-*s", widths[i], cell)
+		}
+	}
+	// Trim trailing whitespace from the last column.
+	line := strings.TrimRight("  "+strings.Join(parts, "  "), " ")
+	_, err := fmt.Fprintln(w, line)
+	return err
+}
